b05b3431200b442923fae6cabb317699debddfe4
Xinlei Xu <x385xu@uwaterloo.ca>
2025-06-25 10:33:06 -0400

    Compute_AIC

---
+++ compute_AIC.R
@@ -0,0 +1,61 @@
+library(netmeta)
+library(nmadb)
+library(dplyr)
+
+# Compute AIC of datasets in nmadb given indices using
+#   multiplicative and random/fixed effect models
+compute_AIC <- function(dat = dat_nmadb, ind) {
+  len <- length(ind)
+  AIC_add <- numeric(len)
+  AIC_fixed <- numeric(len)
+  AIC_mul <- numeric(len)
+  
+  for (j in seq_along(ind)) {
+    i <- ind[j]
+    
+    net <- runnetmeta(dat_nmadb$recid[i])
+    #observed TE
+    theta <- net$TE
+    m <- net$m
+    n <- net$n
+    V <- diag(net$seTE^2)
+    
+    # Multiplicative effect
+    theta_me <- net$TE.nma.fixed # fitted TE
+    phi <- as.numeric(t(theta-theta_me) %*% 
+                        solve(V) %*% (theta-theta_me) / (m-n+1))
+    logL_me <- -0.5*(m*log(2*pi)+
+                        log(det(phi*V))+ 
+                        t(theta-theta_me) %*% solve(phi*V) %*% (theta-theta_me))
+    AIC_mul[j] <- 2*n - 2*logL_me
+    
+    # Additive effects
+    tau_hat <- net$tau
+    sig <- V+tau_hat^2*diag(m)
+    theta_ae <- net$TE.nma.random
+    logL_ae <- -0.5*(m*log(2*pi)+
+                       log(det(sig))+ 
+                       t(theta-theta_ae) %*% solve(sig) %*% (theta-theta_ae))
+    AIC_add[j] <- 2*n-2*logL_ae
+    
+    # Fixed effect
+    sig <- V
+    theta_fe <- net$TE.nma.fixed
+    logL_fe <- -0.5*(m*log(2*pi)+
+                       log(det(V))+ 
+                       t(theta-theta_fe) %*% solve(V) %*% (theta-theta_fe))
+    AIC_fixed[j] <- 2*(n-1)-2*logL_fe
+    
+    
+  }
+  return(list(
+    recid =  dat$recid[ind],
+    AIC_add  = AIC_add,
+    AIC_fixed = AIC_fixed,
+    AIC_mul  = AIC_mul,
+    AIC_mul_add = AIC_mul - AIC_add,
+    AIC_mul_fixed = AIC_mul - AIC_fixed
+  ))
+}
+
+
